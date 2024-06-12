01298c63919b25c4dfc21f00de5e307006fea686
RudolfRukavina <rukavina.rudi@gmail.com>
2024-06-12 15:43:27 +0200

    Hyperlink taxi website v0.05

--- components/Footer.vue
+++ components/Footer.vue
@@ -88,7 +88,7 @@
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
       <div class="text-center">
         <a href="#" class="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+          <img src="../assets/img/hplink.webp" class="hgi-6 mr-3 sm:h-9" alt="Landwind Logo" />
           Landwind
         </a>
         <span class="block text-sm text-center text-gray-500 dark:text-gray-400">© 2021-2022 Landwind™. All Rights
