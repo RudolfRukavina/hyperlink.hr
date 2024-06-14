5fb61bb25371049468db783fa5c5a192c5f8c510
RudolfRukavina <rukavina.rudi@gmail.com>
2024-06-14 13:53:46 +0200

    Hyperlink taxi website v0.36

--- pages/385976415557/Dev.vue
+++ pages/385976415557/Dev.vue
@@ -51,7 +51,7 @@
           <ul class="py-2 font-medium" role="none">
             <li>
               <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 role="menuitem">
                 <div class="inline-flex items-center">
                   <svg aria-hidden="true" class="h-3.5 w-3.5 rounded-full me-2" xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +77,7 @@
             </li>
             <li>
               <a href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 role="menuitem">
                 <div class="inline-flex items-center">
                   <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -159,7 +159,7 @@
           </ul>
         </div>
         <button data-collapse-toggle="navbar-language" type="button"
-          class="inline-flex items-center p-2 w-10 h-10 bg-black justify-center text-sm text-white rounded-lg md:hidden hover:bg-black focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 w-10 h-10 bg-black justify-center text-sm text-white rounded-lg md:hidden hover:bg-black focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-language" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -187,7 +187,11 @@
           </li>
           <li>
             <a href="#"
-              class="block py-2 px-3 md:p-0 text-gray-100 rounded hover:bg-white hover:text-black md:hover:bg-transparent md:hover:underline underline-offset-2 md:hover:text-white ">Contact</a>
+              class="block py-2 px-3 md:p-0 text-gray-100 rounded hover:bg-white hover:text-black md:hover:bg-transparent md:hover:underline underline-offset-2 md:hover:text-white ">Gallery</a>
+          </li>
+          <li>
+            <a href="#"
+              class="block py-2 px-3 md:p-0 text-gray-100 rounded hover:bg-white hover:text-black md:hover:bg-transparent md:hover:underline underline-offset-2 md:hover:text-white ">Reservations</a>
           </li>
         </ul>
       </div>
@@ -451,7 +455,7 @@
                   required />
               </div>
               <div class="sm:col-span-2">
-                <label for="message" class="block mb-2 text-sm font-medium text-white dark:text-gray-400">Vaša
+                <label for="message" class="block mb-2 text-sm font-medium text-white">Vaša
                   poruka</label>
                 <textarea id="message" rows="10"
                   class="block p-2.5 w-full text-[16px] text-black bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-white focus:border-white"
@@ -527,6 +531,197 @@
         </div>
       </section>
     </div>
+
+    <footer class="bg-black lg:grid lg:grid-cols-5">
+      <div class="lg:col-span-2">
+        <div class="bg-black pt-5">
+          <div class="flex px-5 w-auto">
+            <div class="w-[100vw] text-center bg-black">
+              <div class="flex">
+                <div
+                  class="inline-block pe-5 dark:ring-white/10 ring-primary/5 dark:shadow-thick rounded-3xl lg:row-start-4 h-full p-5 dark:bg-secondary">
+                  <div class="flex justify-between cursor-pointer">
+                    <p class="text-4xl tracking-tight font-medium text-white text-start md:text-5xl">
+                      {{ taxiName }}
+                    </p>
+                  </div>
+                  <div class="flex items-center mb-2 w-[80vw] min-w-md max-w-lg">
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor" viewBox="0 0 22 20">
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor" viewBox="0 0 22 20">
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor" viewBox="0 0 22 20">
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor" viewBox="0 0 22 20">
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor" viewBox="0 0 22 20">
+                      <path
+                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <p class="ms-1 text-sm font-medium text-gray-500">
+                      5
+                    </p>
+                    <p class="ms-1 text-sm font-medium text-gray-500">
+                      out of
+                    </p>
+                    <p class="ms-1 text-sm font-medium text-gray-500">
+                      5
+                    </p>
+                  </div>
+                  <p class="text-base text-start font-medium text-gray-200">
+                    <span class="text-xl">{{ ratings }}</span> global ratings
+                  </p>
+                  <div class="flex items-center mt-4">
+                    <a href="#" class="text-base font-medium text-gray-300 hover:underline">5
+                      star</a>
+                    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                      <div class="h-5 bg-yellow-300 rounded" style="width: 100%"></div>
+                    </div>
+                    <span class="text-base font-medium text-gray-200">100%</span>
+                  </div>
+                  <div class="flex items-center mt-4">
+                    <a href="#" class="text-sm font-medium text-gray-300 hover:underline">4 star</a>
+                    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                      <div class="h-5 bg-yellow-300 rounded" style="width: 0%"></div>
+                    </div>
+                    <span class="text-sm font-medium text-gray-200">0%</span>
+                  </div>
+                  <div class="flex items-center mt-4">
+                    <a href="#" class="text-sm font-medium text-gray-300 hover:underline">3 star</a>
+                    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                      <div class="h-5 bg-yellow-300 rounded" style="width: 0%"></div>
+                    </div>
+                    <span class="text-sm font-medium text-gray-200">0%</span>
+                  </div>
+                  <div class="flex items-center mt-4">
+                    <a href="#" class="text-sm font-medium text-gray-300 hover:underline">2 star</a>
+                    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                      <div class="h-5 bg-yellow-300 rounded" style="width: 0%"></div>
+                    </div>
+                    <span class="text-sm font-medium text-gray-200">0%</span>
+                  </div>
+                  <div class="flex items-center mt-4">
+                    <a href="#" class="text-sm font-medium text-gray-300 hover:underline">1 star</a>
+                    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                      <div class="h-5 bg-yellow-300 rounded" style="width: 0%"></div>
+                    </div>
+                    <span class="text-sm font-medium text-gray-200">0%</span>
+                  </div>
+
+                  <div class="md:grid md:grid-cols-2 gap-2 lg:grid-cols-1 text-start">
+                    <div class="mt-4">
+                      <p class="text-sm text-white">
+                        {{ review }}
+                      </p>
+                      <p class="text-xs mt-2 text-zinc-200">
+                        <span class="block text-xs">{{ reviewName }}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="px-10 py-16 sm:px-6 lg:col-span-3 lg:px-8">
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
+          <div>
+            <p>
+              <span class="text-base uppercase tracking-wide text-gray-100">
+                Call us
+              </span>
+
+              <a href="#"
+                class="block text-2xl font-medium text-yellow-300 undeline underline-offset-2 hover:opacity-75 sm:text-3xl dark:text-white">
+                +385 97 641 5557
+              </a>
+            </p>
+
+            <ul class="mt-8 space-y-1 text-3xl text-gray-200">
+              <li>0 - 24</li>
+              <li>We are open every day!</li>
+            </ul>
+
+            <p class="font-semibold mt-5 text-2xl text-white">Pratite nas</p>
+            <ul class="mt-3 flex gap-6">
+              <li>
+                <a href="#" rel="noreferrer" target="_blank" class="text-gray-200 transition hover:opacity-75">
+                  <span class="sr-only">Facebook</span>
+
+                  <svg class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path fill-rule="evenodd"
+                      d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" rel="noreferrer" target="_blank" class="text-gray-200 transition hover:opacity-75">
+                  <span class="sr-only">Instagram</span>
+
+                  <svg class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path fill-rule="evenodd"
+                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="grid grid-cols-1 gap-4">
+            <div>
+              <p class="font-medium text-3xl text-white">Obrt za cestovni promet M.T.</p>
+
+              <ul class="mt-6 space-y-4 text-sm">
+                <li>
+                  <a href="#" class="text-white transition hover:opacity-75">
+                    Jože Šurana 12, 52440 Poreč, Croatia
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-white transition hover:opacity-75">
+                    +385 91 7339 726
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-white transition hover:opacity-75">
+                    lanternataxi.porec@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-12 border-t border-gray-100 pt-4 dark:border-gray-800">
+          <div class="sm:flex sm:items-center sm:justify-center">
+
+            <p class="mt-8 text-base text-center text-white sm:mt-0">
+              &copy; 2024. Obrt za cestovni prijevoz M.T. <br /> All rights reserved. <br />
+              <span class="font-bold mt-2 underline underline-offset-2">Obavijest o zaštiti privatnosti i GDPR-u.</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   </main>
 </template>
 
